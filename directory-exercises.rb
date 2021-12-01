6ea26e91916897bc7b9adc57b12824ab5cc5b3d7
Jason Humbert <jasonhumbert@outlook.com>
2021-12-01 23:17:30 +0000

    Add more student information. (5)

--- directory-exercises.rb
+++ directory-exercises.rb
@@ -7,10 +7,29 @@ def input_students
   name = gets.chomp
   # while the name is not empty, repeat this code.
   while !name.empty? do
+    # add student hobbies.
+    puts "Enter #{name}'s hobbies"
+    puts "To finish, just hit return twice"
+    hobbies = []
+    # get first hobby.
+    hobby = gets.chomp.downcase
+    while !hobby.empty? do
+      hobbies << hobby
+      puts "#{name} has #{hobbies.count} hobbies"
+      # get another hobby from the user.
+      hobby = gets.chomp.downcase
+    end
+
+    # add student country of birth.
+    puts "Enter #{name}'s country of birth"
+    country_of_birth = gets.chomp.capitalize
+
     # add the student hash to the array.
-    students << {name: name, cohort: :november}
+    students << {name: name, cohort: :november, hobbies: hobbies, country_of_birth: country_of_birth, height: nil}
     puts "Now we have #{students.count} students"
     # get another name from the user
+    puts "Please enter the name of the next student"
+    puts "Or, hit return to finish"
     name = gets.chomp
   end
   # return the array of students.
@@ -27,7 +46,7 @@ def print(students)
   letter = gets.chomp.upcase
 
   i, number = 0, 1
-  while i < students.length do
+  while i < students.count do
     if students[i][:name].chr == letter and students[i][:name].length < 12
       puts "#{number}. #{students[i][:name]} (#{students[i][:cohort]} cohort)"
       number += 1
